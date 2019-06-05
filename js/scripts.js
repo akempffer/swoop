530d192f8537d41ed4e64573443731e174df05c0
Alyssa Kempffer <akempffer@eagles.ewu.edu>
2019-06-05 11:14:45 -0700

    Added chocolat elements to CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 console.log('OK');
 
+//page headings
 $('h1').lettering();
+
+// Portfolio
+$('.portfolio').Chocolat({
+
+enableZoom : false, 
+separator2 : '👨‍💻', 
+
+});
+
+//css grid generator and codepen.io.topics - remember to cite!
